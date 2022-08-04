64889016cc9b0b56b969923660f55c87d002cb83
lucasbaquinoo <lucasbaquino@icloud.com>
2022-08-03 21:03:05 -0300

    docs: remove old app

--- apps/docs/next.config.js
+++
@@ -1,9 +0,0 @@
-const ntm = require('next-transpile-modules')
-
-module.exports = ntm([
-  '@vercel/examples-ui',
-  '@acme/design-system',
-  '@acme/pages',
-])({
-  basePath: '/docs',
-})
--- apps/docs/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-const { getContent } = require('@acme/tailwind-config/content')
-
-module.exports = {
-  presets: [require('@acme/tailwind-config')],
-  content: getContent([
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ]),
-}
